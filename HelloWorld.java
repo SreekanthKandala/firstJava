20ff1721aecd82bdfa9c116c62ef2e88515615ee
SreekanthKandala <105445924+SreekanthKandala@users.noreply.github.com>
2022-05-12 21:16:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,9 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
-}
+public class ReplaceString{  
+public static void main(String args[]){  
+String s1="Hi Skand Welcome to Devops ";  
+String replaceString=s1.replace("Skand","Sreekanth");//replaces all occurrences of "Skand" to "Skand"  
+System.out.println(replaceString);  
+}}  
